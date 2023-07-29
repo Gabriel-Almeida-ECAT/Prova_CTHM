fb63e6347c72dcba1da39737d6858e6a989b3608
Gabriel Almeida <gabriel.oliveira.ga76@gmail.com>
2023-07-29 15:06:16 -0400

    initial commit + Q5

---
+++ questao_5/Q5.py
@@ -0,0 +1,20 @@
+import datetime
+
+from datetime import datetime
+from flask import Flask, request, jsonify
+
+server = Flask(__name__)
+
+@server.before_request
+def log_request_info():
+    with open("request_time_log.log", "a") as log_file:
+        current_time = datetime.now().strftime("%Y.%M.%d %H:%M:%S")
+        log_file.write(f"{current_time} - Resquest: {request.method}\n")
+
+# Rota básica
+@server.route('/save_resquest_time')
+def index():
+    return jsonify({"Request saved in file": "request_time_log"})
+
+if __name__ == '__main__':
+    server.run()
